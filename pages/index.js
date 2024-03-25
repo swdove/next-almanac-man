e182d9722c78038affa6effee370269455c73969
Sean Dove <swdove@gmail.com>
2024-03-25 13:37:35 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
+          <p>hello world</p>
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
