d192ed3fdb28d53661f8c9e57dddf966b878bc62
Sean Dove <swdove@gmail.com>
2024-03-25 15:25:02 -0600

    add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   content: [    
     './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
-    './pages/**/*.{js,ts,jsx,tsx,mdx}'],
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
   theme: {
     extend: {},
   },
